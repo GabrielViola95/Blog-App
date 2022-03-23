6ea904cf6f15d3f9a4a54fc8d7c5fd01c1d99c5a
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-03-23 20:49:33 -0300

    home page in progress

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {collection, getDocs} from 'firebase/firestore'
+import { db } from '../../firebase-config';
+
 
 function Home() {
+  const [postList, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, [deletePost]);
   return (
     <div>
         <h1>Home</h1>
